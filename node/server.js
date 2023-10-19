6b43fbc96738c87f352c371c45a1d2f9de839315
MabJo <1931631@cegeplimoilou.ca>
2023-10-19 15:41:07 -0400

    Partie 1 - Détruire et créer les tables Utilisateur et Evenements

--- node/server.js
+++ node/server.js
@@ -2,9 +2,48 @@ const mysql = require("mysql");
 const connString = "mysql://urser-tp2:AVNS_K1Z7ax2UdEVbfuAtfQv@mysql-tp2-sm-tr-monptitdoigt29-4875.aivencloud.com:16151/tp2bd?ssl-mode=REQUIRED";
 
 let conn=mysql.createConnection(connString);
+let sql;
 
 conn.connect(err => {
     if(err) throw err;
     console.log("Connexion à la base de données tp2 !");
+
+    //DROP TABLE IF EXISTS
+    sql = "DROP TABLE IF EXISTS evenements";
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log("Table Evenements détruite ❌");
+    })
+
+     sql = "DROP TABLE IF EXISTS utilisateurs";
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log("Table Utilisateurs détruite ❌");
+    })
+
+
+    //CREATE TABLE
+    sql = "CREATE TABLE utilisateurs"+
+        " (Id INT not null AUTO_INCREMENT, "+
+        " Full_Name VARCHAR(255), "+
+        " Mot_De_Passe VARCHAR(255), "+
+        " PRIMARY KEY (Id) )";
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log("Table Utilisateurs créée 👍");
+    })
+
+    sql = "CREATE TABLE evenements"+
+        " (Id INT not null AUTO_INCREMENT, "+
+        " Titre VARCHAR(255), "+
+        " Date_event DATE, "+
+        " Id_Utilisateur INT, "+
+        " PRIMARY KEY (Id), "+
+        " FOREIGN KEY (Id_Utilisateur) REFERENCES utilisateurs (Id) ON DELETE CASCADE)";
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log("Table Evenements créée 👍");
+    })
+
 
 });
