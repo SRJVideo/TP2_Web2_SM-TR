b9632d34a215a1f4720a98cdd2233d4b67940963
MabJo <1931631@cegeplimoilou.ca>
2023-11-02 01:22:10 -0400

    Rendus primaires des composant Connecter et Inscrire

--- reactapp/src/components/Connecter.js
+++ reactapp/src/components/Connecter.js
@@ -1,12 +1,35 @@
 import React, {Component} from 'react';
+import Form from 'react-bootstrap/Form';
+import {Button} from "react-bootstrap";
+import {NavLink} from "react-router-dom";
 
 class Connecter extends Component {
     render() {
         return (
             <div>
-            <h1>Se Connecter</h1>
-            <p>Page de connexion</p>
-        </div>
+                <h1>Se Connecter</h1>
+                <p>Entrez vos identifiants</p>
+
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="username">Nom d'utilisateur</Form.Label>
+                        <Form.Control name="username" type="text"></Form.Control>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="password">Mot de passe</Form.Label>
+                        <Form.Control name="password" type="password"></Form.Control>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Text muted>Vons n'avez pas de compte ?&nbsp;
+                           <NavLink to="/inscrire">Inscrivez-vous</NavLink>
+                            &nbsp;ici</Form.Text>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Continuer
+                    </Button>
+                </Form>
+            </div>
         );
     }
 }
--- reactapp/src/components/Inscrire.js
+++ reactapp/src/components/Inscrire.js
@@ -1,15 +1,44 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
+import {NavLink} from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import {Button} from "react-bootstrap";
 
-class Inscrire extends Component {
-    render() {
+function Inscrire() {
+
+
         return (
             <div>
-            <h1>S'inscrire</h1>
-            <p>Page d'inscription
-            </p>
-        </div>
+                <h1>S'inscrire</h1>
+                <p>Créer un nouvel utilisateur</p>
+
+
+                <Form >
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="username">Nom d'utilisateur</Form.Label>
+                        <Form.Control name="username" type="text" required></Form.Control>
+                        <Form.Control.Feedback type="invalid">Veuillez entrez au moins un caractère alphanumérique</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label htmlFor="password">Mot de passe</Form.Label>
+                        <Form.Control name="password" type="password"required></Form.Control>
+                        <Form.Control.Feedback type="invalid">Veullez entrer un mot de passe avec au moins 4 caractères</Form.Control.Feedback>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Text>
+                            Si votre compte existe déjà,&nbsp;
+                            <NavLink to="/connecter">Connectez-vous</NavLink>
+                            &nbsp;ici
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit">
+                        Continuer
+                    </Button>
+                </Form>
+
+            </div>
         );
-    }
+
 }
 
 export default Inscrire;
